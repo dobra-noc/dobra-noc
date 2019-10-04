d77a5c592403675ac7d41288b2c2475e475bf21d
MagicznyPingwin <smolen54321@gmail.com>
2019-10-04 13:50:18 +0200

    Create model, add validation and assign location with env variables

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Location < ApplicationRecord
   has_many :equivalent_continuous_sound_levels
 
-  validates :latitude, uniqueness: { scope: [:latitude, :longitude] }
+  validates :latitude, uniqueness: { scope: %i[latitude longitude] }
 end
