ddd060bb428a0093ecd67e602eef96ed993747f2
Krystian Smoleń <krystian@iMac-Krystian.local>
2019-10-10 11:53:28 +0200

    add route to get records by date and day time

--- app/controllers/api/v1/equivalent_continuous_sound_levels_controller.rb
+++ app/controllers/api/v1/equivalent_continuous_sound_levels_controller.rb
@@ -1,13 +1,32 @@
 module Api
   class V1::EquivalentContinuousSoundLevelsController < ApplicationController
-    def index
-      data = Location.last.equivalent_continuous_sound_levels.hour
-      render json: data.as_json(only: [:start_at, :laeq], :include => { location: { only: :address } })
-    end
+    # def index
+    #   data = Location.last.equivalent_continuous_sound_levels.hour
+    #   render json: data.as_json(only: [:start_at, :laeq], :include => { location: { only: :address } })
+    # end
 
     def show
       data = Location.find(params[:id]).equivalent_continuous_sound_levels.hour
       render json: data.as_json(only: [:start_at, :laeq], :include => { location: { only: :address } })
+    end
+
+    def get_by_day
+      data = Location.find(params[:id]).equivalent_continuous_sound_levels.hour.where(
+        start_at: return_day_with_day_time(params[:day], params[:day_time])
+      )
+
+      render json: data.as_json(only: [:start_at, :laeq], :include => { location: { only: :address } })
+    end
+
+    private
+
+    def return_day_with_day_time(day, day_time)
+      day = day.to_date
+      if day_time == 'night'
+        day + 22.hours .. day + 1.day + 6.hours
+      elsif day_time == "day"
+        day + 6.hours .. day + 22.hours
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'home/index'
   namespace :api do
     namespace :v1 do
-      resources :equivalent_continuous_sound_levels, only: [:index, :show]
+      resources :equivalent_continuous_sound_levels, only: :show
+      get 'equivalent_continuous_sound_levels/:id/:day/:day_time', :to => 'equivalent_continuous_sound_levels#get_by_day'
       resources :locations, only: :index
     end
   end
