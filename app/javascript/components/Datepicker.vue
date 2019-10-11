7cb25cf4405b92cbba9483a0ef61f65626f461c4
Krystian Smoleń <krystian@iMac-Krystian.local>
2019-10-11 10:36:23 +0200

    fix code quality with prettier

--- app/javascript/components/Datepicker.vue
+++ app/javascript/components/Datepicker.vue
@@ -1,44 +1,49 @@
 <template>
   <v-date-picker
-    v-model='selectedValue'
-    :disabled-dates='[ { start: null, end: null}]'
-    :available-dates='availableDates'
+    v-model="selectedValue"
+    :disabled-dates="[{ start: null, end: null }]"
+    :available-dates="availableDates"
     @input="sendDate(selectedValue)"
-    :max-date="new Date(availableDates[availableDates.length-1])"
+    :max-date="new Date(availableDates[availableDates.length - 1])"
     :min-date="new Date(availableDates[0])"
-    is-inline>
+    is-inline
+  >
   </v-date-picker>
 </template>
 
 <script>
-  import { setupCalendar, DatePicker} from 'v-calendar'
-  import 'v-calendar/lib/v-calendar.min.css'
+import { setupCalendar, DatePicker } from "v-calendar";
+import "v-calendar/lib/v-calendar.min.css";
 
-  setupCalendar({
-    firstDayOfWeek: 2
-  })
+setupCalendar({
+  firstDayOfWeek: 2
+});
 
-  export default {
-    components: { 'v-date-picker' : DatePicker },
-    props: { availableDates: Array },
-    methods: {
-      sendDate (date) {
-        this.$emit('date-value', date)
-      }
-    },
-    data() {
-      return {
-        selectedValue: new Date(),
-        attrs: [
+export default {
+  components: {
+    "v-date-picker": DatePicker
+  },
+  props: {
+    availableDates: Array
+  },
+  methods: {
+    sendDate(date) {
+      this.$emit("date-value", date);
+    }
+  },
+  data() {
+    return {
+      selectedValue: new Date(),
+      attrs: [
         {
-          key: 'today',
+          key: "today",
           dates: "19-10-2019",
           highlight: {
-            backgroundColor: '#ff8080',
+            backgroundColor: "#ff8080"
           }
         }
-      ],
-      }
-    },
+      ]
+    };
   }
+};
 </script>
