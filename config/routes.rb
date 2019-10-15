625e5f88ac153341f2303401698dadf648a5ad8d
MagicznyPingwin <smolen54321@gmail.com>
2019-10-15 11:25:22 +0200

    Update rails to 6.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'home/index'
   namespace :api do
     namespace :v1 do
       resources :equivalent_continuous_sound_levels, only: :show
-      get 'equivalent_continuous_sound_levels/:id/:day', :to => 'equivalent_continuous_sound_levels#get_by_day'
+      get 'equivalent_continuous_sound_levels/:id/:day', to: 'equivalent_continuous_sound_levels#get_by_day'
       resources :locations, only: :index
     end
   end
