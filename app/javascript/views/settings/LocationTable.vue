89b49decebdfd6949cc37a3432355652a3bd3645
MagicznyPingwin <smolen54321@gmail.com>
2019-10-21 13:15:45 +0200

    create settings page

---
+++ app/javascript/views/settings/LocationTable.vue
@@ -0,0 +1,24 @@
+<template>
+<table class="table table-striped">
+					<thead>
+						<tr>
+							<th scope="col">Address</th>
+							<th scope="col">Latitude</th>
+							<th scope="col">Longitude</th>
+							<th scope="col">Description</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th scope="row">1</th>
+							<td>Mark</td>
+							<td>Otto</td>
+							<td>@mdo</td>
+						</tr>
+					</tbody>
+				</table>
+</template>
+
+<script>
+
+</script>
