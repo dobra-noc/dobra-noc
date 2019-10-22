2a3ad23fff91fb9d887c0107fde4cdc6167b2736
MagicznyPingwin <smolen54321@gmail.com>
2019-10-22 10:14:26 +0200

    display location in table

--- app/javascript/views/settings/LocationTable.vue
+++ app/javascript/views/settings/LocationTable.vue
@@ -5,20 +5,56 @@
 							<th scope="col">Address</th>
 							<th scope="col">Latitude</th>
 							<th scope="col">Longitude</th>
-							<th scope="col">Description</th>
 						</tr>
 					</thead>
 					<tbody>
-						<tr>
-							<th scope="row">1</th>
-							<td>Mark</td>
-							<td>Otto</td>
-							<td>@mdo</td>
+						<tr
+						  v-for="location in locations"
+						  @click="sendLocation(location)"
+						>
+							<th scope="row">{{location.address}}</th>
+							<td>{{location.latitude}}</td>
+							<td>{{location.longitude}}</td>
 						</tr>
 					</tbody>
 				</table>
 </template>
-
 <script>
-
+import axios from 'axios'
+				axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+				export default {
+					data() {
+						return {
+							locations: null,
+							location: null
+						}
+					},
+					methods: {
+						sendLocation: function (location) {
+							this.location = location
+							axios.post('/api/v1/settings/location', {
+								setting: {
+									address: this.location['address'],
+									description: this.location['description'],
+									longitude: this.location['longitude'],
+									latitude: this.location['latitude'],
+									is_recording: this.location['is_recording']
+								}
+							}).then(function (response) {
+								console.log(response);
+							}).catch(function (error) {
+								console.log(error);
+							});
+						},
+					},
+					async mounted() {
+						this.loaded = false;
+						try {
+							await fetch("/api/v1/locations").then(response => response.json()).then(data => (this.locations = data));
+							this.loaded = true;
+						} catch (e) {
+							console.log(e);
+						}
+					}
+				}
 </script>
