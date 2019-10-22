206ac702481649aaf65980494bb45ab2fd5f08bc
MagicznyPingwin <smolen54321@gmail.com>
2019-10-22 11:33:25 +0200

    add changes after review

--- app/controllers/api/v1/equivalent_continuous_sound_levels_controller.rb
+++ app/controllers/api/v1/equivalent_continuous_sound_levels_controller.rb
@@ -2,11 +2,6 @@
 
 module Api
   class V1::EquivalentContinuousSoundLevelsController < ApplicationController
-    # def index
-    #   data = Location.last.equivalent_continuous_sound_levels.hour
-    #   render json: data.as_json(only: [:start_at, :laeq], :include => { location: { only: :address } })
-    # end
-
     def show
       data = Location.find(params[:id]).equivalent_continuous_sound_levels.hour
       dates = Location.find(params[:id])
